4777060fe56cba6c104f8b10c01634181eae5df2
KasraMohammadpour <kasramohammadpor@nematifamilyfundation.onmicrosoft.com>
2024-02-22 23:26:04 +0330

    remove data from constants

--- constants/data.ts
+++ constants/data.ts
@@ -1,38 +1,3 @@
-import type { UserInfo } from "@/services/types";
-
-export const data: UserInfo[] = [
-  {
-    id: "m5gr84i9",
-    full_name: "kasra",
-    phone: "1234567882",
-    email: "ken99@yahoo.com",
-  },
-  {
-    id: "3u1reuv4",
-    full_name: "amir",
-    phone: "9876543278",
-    email: "Abe45@gmail.com",
-  },
-  {
-    id: "derv1ws0",
-    full_name: "nader",
-    phone: "74185296352",
-    email: "Monserrat44@gmail.com",
-  },
-  {
-    id: "5kma53ae",
-    full_name: "sam",
-    phone: "75315968496",
-    email: "Silas22@gmail.com",
-  },
-  {
-    id: "bhqecj4p",
-    full_name: "iran",
-    phone: "86247531978",
-    email: "carmella@hotmail.com",
-  },
-];
-
 export const dataMap = {
   "ken99@yahoo.com": {
     id: "m5gr84i9",
@@ -64,4 +29,4 @@ export const dataMap = {
     phone: "86247531978",
     email: "carmella@hotmail.com",
   },
-};
+} as const;
